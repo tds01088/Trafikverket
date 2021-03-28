3c2aabed7a7a01436170ba6e935ad7a314fa36de
Zana Ali <zana.ali@outlook.com>
2021-03-28 15:16:42 +0200

    Delete Elev_Betyg_1.sql

--- Trafikverket.Database/dbo/Tables/Elev_Betyg_1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Elev_Betyg] (
-    [id]      INT  IDENTITY (1, 1) NOT NULL,
-    [elev_id] INT  NULL,
-    [test]    TEXT NULL,
-    [betyg]   INT  NULL,
-    PRIMARY KEY CLUSTERED ([id] ASC)
-);
-
