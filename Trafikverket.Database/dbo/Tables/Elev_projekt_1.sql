e57625389066ee79add03c4f73213d88a5d31cb3
sezzaal <zana.ali@outlook.com>
2021-03-28 15:04:53 +0200

    update Database

---
+++ Trafikverket.Database/dbo/Tables/Elev_projekt_1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Elev_projekt] (
+    [id]      INT  IDENTITY (1, 1) NOT NULL,
+    [elev_id] INT  NULL,
+    [title]   TEXT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ Trafikverket.Database/dbo/Views/V_Elever_betyg_1.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW dbo.V_Elever_betyg
+AS
+SELECT        elev.firstname, elev.lastname, betyg.betyg, projekt.title
+FROM            dbo.Elev AS elev FULL OUTER JOIN
+                         dbo.Elev_Betyg AS betyg ON elev.id = betyg.elev_id FULL OUTER JOIN
+                         dbo.Elev_projekt AS projekt ON elev.id = projekt.elev_id
